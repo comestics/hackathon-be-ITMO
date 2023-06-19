e1d10860dabc09aa495ad8f4a5b7aa67a1e06b34
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-06-19 22:13:57 +0300

    setup colors

---
+++ src/constants/config.ts
@@ -0,0 +1,10 @@
+const config = {
+  baseUrlApi: 'https://risenvn.space/api/',
+  baseUrl: 'http://localhost:3000'
+  // imageURL: (url?: string) => (url ? '' + url : ''),
+  // percentVAT: 10,
+  // commentSymbol: '--rating-'
+  // maxSizeUploadAvatar: 1048576
+}
+
+export default config
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,78 @@
-const plugin = require("tailwindcss/plugin");
+const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      colors: {
+        base: {
+          black: '#333333',
+          white: '#FFFFFF'
+        },
+        primary: {
+          blue: '#1846C7',
+          blue24: '#C8D3F2',
+          blue8: '#DAE1F6',
+          pink: '#EE215B',
+          pink24: '#FCD3DE',
+          pink8: '#FDE9EE'
+        },
+        secondary: {
+          blue: '#0091FF',
+          purple: '#A50AFF',
+          green: '#22B217',
+          red: '#E02E2E',
+          orange: '#FA6400',
+          yellow: '#F7B500'
+        },
+        secondary_light: {
+          blue_light: '#E5F4FF',
+          purple_light: '#F6E6FF',
+          green_light: '#E9F7E8',
+          red_light: '#FCEAEA',
+          orange_light: '#FFEFE5',
+          yello_light: '#FEF8E5'
+        },
+        gray: {
+          gray80: '#5C5C5C',
+          gray60: '#858585',
+          gray40: '#ADADAD',
+          gray12: '#E7E7E7',
+          gray4: '#F7F7F7'
+        },
+        non_style: {
+          gray20: '#D6D6D6',
+          gray8: '#EFEFEF',
+          blue_hover: '#1846C7',
+          blue_active: '#143AA4',
+          deep_sky: '#00C2FF',
+          mint: '#300FFA3',
+          drak_green: '#008D6B',
+          violet_red: '#D4007D'
+        }
+      }
+    }
   },
   plugins: [
     plugin(function ({ addComponents, theme }) {
       addComponents({
-        ".container": {
-          maxWidth: theme("columns.7xl"),
-          marginLeft: "auto",
-          marginRight: "auto",
-          paddingLeft: theme("spacing.4"),
-          paddingRight: theme("spacing.4"),
-        },
-      });
-    }),
-  ],
-};
+        '.container': {
+          maxWidth: theme('columns.7xl'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4'),
+          paddingRight: theme('spacing.4')
+        }
+      })
+    })
+  ]
+}
