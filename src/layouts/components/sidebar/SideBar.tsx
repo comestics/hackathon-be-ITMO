71907b3b2299854b0e8a5a03cf718e5dfe76f56b
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-06-20 00:14:50 +0300

    add icons

--- src/layouts/components/sidebar/SideBar.tsx
+++ src/layouts/components/sidebar/SideBar.tsx
@@ -2,6 +2,44 @@ import Image from 'next/image'
 import React from 'react'
 
 const SideBar = () => {
+  const linkSideBar = [
+    { title: 'Главная', link: '/home', icon: '/home.svg' },
+    { title: 'Расписание', link: '/home', icon: '/schedule.svg' },
+    { title: 'Учебный план', link: '/home', icon: '/adviser.svg' },
+    {
+      title: 'Запись по выбору',
+      link: '/home',
+      // children: [
+      //   { title: 'Дашборд', link: '/home' },
+      //   { title: 'Ментальная помощь', link: '/home' }
+      // ],
+      icon: '/menu-file.svg'
+    },
+    {
+      title: 'Спорт',
+      link: '/home',
+      // children: [
+      //   { title: 'Дашборд', link: '/home' },
+      //   { title: 'Ментальная помощь', link: '/home' }
+      // ]
+      icon: '/kronbar.svg'
+    },
+    {
+      title: 'Состояние',
+      link: '/home',
+      children: [
+        { title: 'Дашборд', link: '/home' },
+        { title: 'Ментальная помощь', link: '/home' }
+      ],
+      icon: '/sticker.svg'
+    },
+    { title: 'Финансы', link: '/home', icon: '/wallet.svg' },
+    { title: 'Персоналии', link: '/home', icon: '/users.svg' },
+    { title: 'Зачётка', link: '/home', icon: '/notebook.svg' },
+    { title: 'Заявки', link: '/home', icon: '/menu-note.svg' },
+    { title: 'Сервисы', link: '/home', icon: '/services.svg' }
+  ]
+
   return (
     <>
       <button
@@ -11,7 +49,7 @@ const SideBar = () => {
         type='button'
         className='inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
       >
-        <span className='sr-only'>Open sidebar</span>
+        {/* <span className='sr-only'>Open sidebar</span> */}
         <svg
           className='w-6 h-6'
           aria-hidden='true'
@@ -20,8 +58,8 @@ const SideBar = () => {
           xmlns='http://www.w3.org/2000/svg'
         >
           <path
-            clip-rule='evenodd'
-            fill-rule='evenodd'
+            clipRule='evenodd'
+            fillRule='evenodd'
             d='M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z'
           ></path>
         </svg>
@@ -32,40 +70,20 @@ const SideBar = () => {
         className='fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0'
         aria-label='Sidebar'
       >
-        <div className='h-full px-8 py-5 overflow-y-auto bg-white '>
-          <div className='flex items-center justify-start '>
+        <div className='h-full py-5 overflow-y-auto bg-white '>
+          <div className='flex items-center justify-start cursor-pointer px-7 '>
             <Image src='/logo.svg' alt='' width={160} height={60}></Image>
           </div>
-          <ul className='mt-6 space-y-2 font-medium'>
-            <li>
-              <a href='#' className='flex items-center text-gray-900 rounded-lg hover:bg-gray-100'>
-                <svg
-                  aria-hidden='true'
-                  className='w-6 h-6 text-gray-500 transition duration-75 d '
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path d='M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z'></path>
-                  <path d='M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z'></path>
-                </svg>
-                <span className='ml-3'>Dashboard</span>
-              </a>
-            </li>
-            <li>
-              <a href='#' className='flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100'>
-                <svg
-                  aria-hidden='true'
-                  className='flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 '
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path d='M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z'></path>
-                </svg>
-                <span className='flex-1 ml-3 whitespace-nowrap'>Kanban</span>
-              </a>
-            </li>
+          <ul className='px-5 mt-6 space-y-2 font-medium'>
+            {linkSideBar.map((itemLink, index) => (
+              <li className='p-2 rounded-md hover:bg-gray-gray4' key={index}>
+                <a href='#' className='flex items-center text-gray-900 rounded-lg '>
+                  <Image src={itemLink.icon} alt='' width={25} height={25}></Image>
+
+                  <span className='ml-3'>{itemLink.title}</span>
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </aside>
